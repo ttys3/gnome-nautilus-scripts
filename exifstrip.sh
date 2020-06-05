f8c8caef8aa6d47351b920f1bab543b7bf4db7e3
荒野無燈 <ttys3@outlook.com>
2020-06-06 03:35:27 +0800

    add exifstrip.sh

---
+++ exifstrip.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+find . \( -iname "*.jpg" -or -iname "*.jpeg" -or -iname "*.png" -or -iname "*.tif" -or -iname "*.bmp" -or -iname "*.gif" -or -iname "*.xpm" -or -iname "*.nef" -or -iname "*.cr2" -or -iname "*.arw" \) \
+-exec gm convert -strip {} {} \;
