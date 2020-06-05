f6385587e0fed0376c79727dcec03037a3b95226
荒野無燈 <ttys3@outlook.com>
2020-06-05 21:05:25 +0800

    add copy-dir-path.py

--- copy-dir-path.py
+++ copy-dir-path.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author: ttyS3
-copy selected file path
+copy selected filename path
 """
 
 import datetime
@@ -13,14 +13,19 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
 
 def main():
+    filenames = []
+    for path in os.getenv('NAUTILUS_SCRIPT_SELECTED_FILE_PATHS','').splitlines():
+        try:
+            filenames.append(os.path.dirname(path))
+        except OSError:
+            continue
     clip = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
-    filenames = os.getenv('NAUTILUS_SCRIPT_SELECTED_FILE_PATHS','').strip()
-    clip.set_text(filenames, -1)
+    clip.set_text("\n".join(filenames), -1)
 
     # MessageDialog just for debug
     md = Gtk.MessageDialog(parent=None, 
     flags=0, message_type=Gtk.MessageType.INFO, 
-    buttons=Gtk.ButtonsType.CLOSE, text=filenames)
+    buttons=Gtk.ButtonsType.CLOSE, text="\n".join(filenames))
     md.run()
     md.destroy()
 
--- copy-filename.py
+++ copy-filename.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author: ttyS3
-copy selected filename
+copy selected file base name
 """
 
 import datetime
